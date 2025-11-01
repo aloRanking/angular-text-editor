4979f440fe78b29d91506a2139c77fa73f4939dd
aloranking <alobently@outlook.com>
2025-11-01 08:28:20 +0100

    feat: working text editor

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,76 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, ElementRef, signal, ViewChild } from '@angular/core';
+
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('text-editor');
+  @ViewChild('editor') editor!: ElementRef;
+
+  constructor(private sanitizer: DomSanitizer) { }
+
+  styles = {
+    bold: false,
+    italic: false,
+    underline: false,
+    color: '#000000'
+  };
+
+  textContent = '';
+  savedContent: SafeHtml[] = [];
+
+
+  applyFormat(command: string, value: string | null = null) {
+
+    this.toggleStyle(command as keyof typeof this.styles);
+
+    document.execCommand(command, false, value ?? '');
+
+    this.editor.nativeElement.focus();
+  }
+
+
+
+  toggleStyle(style: keyof typeof this.styles) {
+    if (style !== 'color') {
+      this.styles[style] = !this.styles[style];
+    }
+  }
+
+  onInput(event: Event) {
+    const element = event.target as HTMLElement;
+    this.textContent = element.innerHTML;
+  }
+
+  save() {
+    const html = this.editor.nativeElement.innerHTML.trim();
+    console.log(html);
+
+    if (html!= '') {
+      const safeHtml: SafeHtml = this.sanitizer.bypassSecurityTrustHtml(html);
+      this.savedContent.push(safeHtml);
+      this.resetEditor();
+    }
+
+
+  }
+
+  resetEditor() {
+    this.editor.nativeElement.innerHTML = '';
+    this.textContent = '';
+    this.styles = {
+      bold: false,
+      italic: false,
+      underline: false,
+      color: '#000000'
+    };
+  }
+
 }
+
